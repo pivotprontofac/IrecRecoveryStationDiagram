d6612625a70b22f18cd31242fc9e4f5c3006198c
pivotprontofac <pivotpronto@gmail.com>
2026-04-04 21:23:36 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RecoveryApp from '../irec-recovery-map.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <RecoveryApp />
+        </React.StrictMode>
+        )
